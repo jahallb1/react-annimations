3dcce75872ec913123e335236f748943e542d3d3
jahallb1 <62314714+jahallb1@users.noreply.github.com>
2021-09-23 17:04:47 -0400

    added button and fixing nav bar

--- src/componets/Navbar.js
+++ src/componets/Navbar.js
@@ -1,28 +1,68 @@
 import React from 'react';
-import styled from "styled-components";
+import styled, {css } from "styled-components/macro";
 import { Link } from "react-router-dom";
+import { menuData } from '../data/MenuData';
+import { Button } from './Button';
 
 const Nav = styled.nav`
-    height: 60px;
-    background-color: red;
+height: 60px;
+display: flex;
+justify-content: space-between;
+padding: 1rem 2rem;
+z-index: 100;
+position: fixed;
+width: 100%;
+background-color: #000;
 `;
 
+const NavLink = css`
+color: #fff;
+display: flex;
+align-items: center;
+padding: 0 1rem;
+cursor: pointer;
+text-decoration: none;
+`
+
 const Logo = styled(Link)`
-  color: #fff;  
+  ${NavLink}
+  font-style: italic; 
 `
 
 const MenuBars = styled.i ``;
-const NavMenu = styled.div ``;
+
+const NavMenu = styled.div `
+display: flex;
+algin-items: center;
+`;
+
+const NavMenuLinks = styled(Link)`
+${NavLink}
+`;
+
+const NavBtn = styled.div`
+display: flex;
+align-items: center;
+margin-right: 24px;
+`;
 
 const Navbar = () => {
     return (
         <Nav>
-            <Logo>EIXL</Logo>
+            <Logo to="/">EIXL</Logo>
             <MenuBars />
             <NavMenu>
-
+                { menuData.map((item, index) => (
+                    <NavMenuLinks to={item.link} key={index}>
+                        {item.title}
+                    </NavMenuLinks>
+                ))}
             </NavMenu>
-           <h1>NavBar</h1> 
+           <NavBtn>
+               <Button to="/contact">
+                   Contact Us
+               </Button>
+           </NavBtn>
         </Nav>
     )
 }
