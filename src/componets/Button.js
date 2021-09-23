fb3663a6cce15bf1e50f9b0519e69b6c9abb5e4e
jahallb1 <62314714+jahallb1@users.noreply.github.com>
2021-09-23 17:21:43 -0400

    made changes to buttons

--- src/componets/Button.js
+++ src/componets/Button.js
@@ -3,6 +3,16 @@ import {Link } from 'react-router-dom';
 
 export const Button = styled(Link)`
 background: ${({ primary }) => (primary ? '#000d1a' : 'CD853F' )}
-
-
+white-space: nowrap;
+outline: none;
+border: none;
+min-width: 100px;
+max-width: 200px;
+cursor: pointer;
+text-decoration: none;
+transition: 0.3s;
+display: flex;
+justify-content: center;
+padding: ${({big}) => (big ? '16px 40px' : '14px 24px') };
+color: ${({primary}) => (primary ? '#fff' : '#000d1a')}
 `
