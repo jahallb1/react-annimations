d75750ede187a61889a43036b73f41cb21f690d6
jahallb1 <62314714+jahallb1@users.noreply.github.com>
2021-09-23 16:26:39 -0400

    implementing router and navbars

--- src/componets/Navbar.js
+++ src/componets/Navbar.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 const Nav = styled.nav`
     height: 60px;
     background-color: red;
 `;
 
-const Logo = styled
+const Logo = styled(Link)`
+  color: #fff;  
+`
+
+const MenuBars = styled.i ``;
+const NavMenu = styled.div ``;
+
 const Navbar = () => {
     return (
         <Nav>
